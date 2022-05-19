d4b65eaf421909be4d4f26724ed0c081f63458da
payalpatra <payalpatra105@gmail.com>
2022-05-19 19:11:33 +0530

    Rendered Data

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import { useQuery } from "react-query"
 
 // Components
+import Item from "./Item/Item"
+
 import Drawer from "@material-ui/core/Drawer";
-import LinearProgress from "@material-ui/core";
+import LinearProgress from "@material-ui/core/LinearProgress";
 import Grid from "@material-ui/core/Grid";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import Badge from "@material-ui/core/Badge";
@@ -14,6 +16,7 @@ import { Wrapper } from "./App.style"
 // Types
 export type CartItemType = {
   id: number;
+  title: string;
   category: string;
   description: string;
   image: string;
@@ -26,14 +29,43 @@ const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch('https://fakestoreapi.com/products')).json();
 
 
-
 const App = () => {
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     'products', getProducts);
 
   console.log(data)
 
-  return <div className="App"> Hye There ! Let's build a Project</div>
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress color="primary" />;
+  if (error) return <div>Soemthing Went Wrong..</div>
+
+
+  return (
+    <div className="App">
+
+      <Wrapper>
+        <Grid container spacing={3} >
+          {
+            data?.map(
+              item => (
+                <Grid item key={item.id} xs={12} sm={4}>
+                  <Item item={item} handleAddToCart={handleAddToCart} />
+                </Grid>
+              )
+            )
+          }
+
+        </Grid>
+      </Wrapper>
+
+    </div>
+  )
+
 }
 
 export default App;
---
+++ src/Item/Item.tsx
@@ -0,0 +1,26 @@
+import Button from "@material-ui/core/Button";
+
+// Types
+import { CartItemType } from "../App";
+
+// Styles 
+import { Wrapper } from "./Item.styles";
+
+type Props = {
+  item: CartItemType;
+  handleAddToCart: (clickedItem: CartItemType) => void;
+}
+
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
+  <Wrapper>
+    <img src={item.image} alt={item.title} />
+    <div>
+      <h3>{item.title}</h3>
+      <p>{item.description}</p>
+      <h3>$ {item.price}</h3>
+    </div>
+    <Button onClick={() => handleAddToCart(item)}>Add To Cart</Button>
+  </Wrapper>
+);
+
+export default Item;
