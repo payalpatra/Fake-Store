4c58eb651fa7b7f592cbc8c9ad9ca27ff24a54f9
payalpatra <payalpatra105@gmail.com>
2022-05-19 18:32:14 +0530

    Fetched Data from API

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,31 @@ import Grid from "@material-ui/core/Grid";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import Badge from "@material-ui/core/Badge";
 
+// Styles
+import { Wrapper } from "./App.style"
+
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  amount: number;
+}
+
+// Fetching Data From API
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
+
 
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products', getProducts);
+
+  console.log(data)
+
   return <div className="App"> Hye There ! Let's build a Project</div>
 }
 
