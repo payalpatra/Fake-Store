8cf51fa758085916fb4ae2827ff6ecf254dd3dff
payalpatra <payalpatra105@gmail.com>
2022-05-19 18:03:15 +0530

    Initial Project Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "react-query"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
+
+
+const App = () => {
+  return <div className="App"> Hye There ! Let's build a Project</div>
 }
 
 export default App;
